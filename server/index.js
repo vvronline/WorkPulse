0de1c658a6bc84ccc02e991beeb0c503527061af
vvronline <vishnuvr19@gmail.com>
2026-02-27 13:26:49 +0530

    Fix Helmet forcing HTTPS on HTTP GCP deployment

--- server/index.js
+++ server/index.js
@@ -21,7 +21,14 @@ const profileRoutes = require('./routes/profile');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+            "upgrade-insecure-requests": null, // Disable forcing HTTPS
+        }
+    }
+}));
 app.use(cors({ origin: process.env.CORS_ORIGIN || 'http://localhost:3000' }));
 app.use(express.json({ limit: '1mb' }));
 
